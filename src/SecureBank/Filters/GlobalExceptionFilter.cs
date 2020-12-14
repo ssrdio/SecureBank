0b3ae36b92ae181bf09bb1fba4640c857ebbebc8
GregorSpagnolo <gregor.spagnolo@gmail.com>
2020-12-14 15:23:26 +0100

    Update GlobalExceptionFilter.cs
    extensions

--- src/SecureBank/Filters/GlobalExceptionFilter.cs
+++ src/SecureBank/Filters/GlobalExceptionFilter.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using SecureBank.Ctf.Models;
 
 namespace SecureBank.Filters
 {
