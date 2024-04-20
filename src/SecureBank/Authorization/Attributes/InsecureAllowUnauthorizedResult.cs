47a4cd9f3dc76753106a892e33e12ea51c987af8
Md Yousuf <yousufzwz@gmail.com>
2024-04-20 15:25:20 +0600

    first code committed

---
+++ src/SecureBank/Authorization/Attributes/InsecureAllowUnauthorizedResult.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SecureBank.Helpers.Authorization.Attributes
+{
+    internal class InsecureAllowUnauthorizedResult : IActionResult
+    {
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            // Introduce vulnerability by always returning a successful result (200 OK)
+            context.HttpContext.Response.StatusCode = 200;
+            return Task.CompletedTask;
+        }
+    }
+}
