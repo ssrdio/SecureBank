ae4a4b81cbde2af762497c6d4fa750f009f80f53
Alex Christakou <alexchristakou@gmail.com>
2024-08-09 15:32:04 +0100

    fix: signup link on login page

--- sb-frontend/src/components/login.tsx
+++ sb-frontend/src/components/login.tsx
@@ -102,7 +102,7 @@ export default function LoginForm({ }: Props) {
           </form>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
+            <Link href="/signup" className="underline">
               Sign up
             </Link>
           </div>
